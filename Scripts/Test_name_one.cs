5cfe59dcbb64a8e329b20bb8344df99ec4fa0943
pbmamani <72666368+pbmamani@users.noreply.github.com>
2021-09-20 15:32:26 -0500

    Create Test_name_one.cs
    esta es una hoja de codigo por favor no la malogres que me demore dias en hacerla

---
+++ Scripts/Test_name_one.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Test_name_one : MonoBehaviour
+{
+    public string name_a;
+
+    public static void Rotate(string name_) {
+        Debug.Log("Do something");
+    }
+}
